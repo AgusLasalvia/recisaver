289f3fc4c45dfe95db1f2763b200ef18384892c2
AgusLasalvia <agus.blumenfeld13@gmail.com>
2025-02-09 23:31:44 -0300

    init

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,19 @@
+
+import { Tabs } from 'expo-router';
+import { SvgXml } from 'react-native-svg';
+import { View, StyleSheet } from 'react-native';
+
+
+const TabLayout = () => {
+	return (
+
+		<></>
+	);
+};
+
+// Estilos personalizados para el icono y el fondo curvado
+const styles = StyleSheet.create({
+
+});
+
+export default TabLayout;
---
+++ mobile/app/signup.tsx
@@ -0,0 +1,21 @@
+import { TouchableWithoutFeedback, StyleSheet } from "react-native"
+
+
+const SignUp = () => {
+
+
+
+
+	return (
+		<TouchableWithoutFeedback>
+
+		</TouchableWithoutFeedback >
+	)
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
+export default SignUp
